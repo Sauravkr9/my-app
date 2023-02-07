b30fb84a60d9691a90a6a6b04acb41dfaa2a5766
Sauravkr9 <msauravkumar9@gmail.com>
2023-02-08 02:38:22 +0530

    Updates

---
+++ precache-manifest.d72f3e88e1ceaf4ace0aa26a649290c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a3c166f38cea9df7bf4",
+    "url": "/my-app/static/css/main.229f284a.chunk.css"
+  },
+  {
+    "revision": "6a3c166f38cea9df7bf4",
+    "url": "/my-app/static/js/main.6a3c166f.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/my-app/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/my-app/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8b16e4071cc3418912ee",
+    "url": "/my-app/static/js/runtime~main.8b16e407.js"
+  },
+  {
+    "revision": "273e4ad1cddb6ad1fdc9d51478c13137",
+    "url": "/my-app/index.html"
+  }
+];
